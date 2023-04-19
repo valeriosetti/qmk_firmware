c4612b10f0b74e28564f1fbe667ff5f3a806fd25
Valerio Setti <vsetti@baylibre.com>
2023-04-19 12:21:51 +0200

    keyboards: add support for the Ergodash-Low keyboard
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

---
+++ keyboards/ergodash_low/rules.mk
@@ -0,0 +1,23 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = qmk-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+NKRO_ENABLE = no
+BACKLIGHT_ENABLE = no
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+DYNAMIC_KEYMAP_ENABLE = no
+SPLIT_KEYBOARD = yes
+BACKLIGHT_DRIVER = no
+DEBUG_ENABLE = no
+OLED_DRIVER = no
